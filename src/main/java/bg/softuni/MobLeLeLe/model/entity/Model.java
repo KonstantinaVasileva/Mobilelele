337fca9481bd54e467dc2a842f2e98e4ab239846
Konstantina Vasileva <konstantinavasileva@gmail.com>
2024-12-11 12:18:39 +0200

    Add initial entity models for Model, Category, and Offer

---
+++ src/main/java/bg/softuni/MobLeLeLe/model/entity/Model.java
@@ -0,0 +1,99 @@
+package bg.softuni.MobLeLeLe.model.entity;
+
+import bg.softuni.MobLeLeLe.model.entity.enums.Category;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "models")
+public class Model {
+    @Id
+    private Long id;
+
+    private String name;
+
+    @Enumerated( jakarta.persistence.EnumType.STRING)
+    private Category category;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "start_year")
+    private int startYear;
+
+    @Column(name = "end_year")
+    private int endYear;
+
+    @Column
+    private LocalDateTime created;
+
+    @Column
+    private LocalDateTime modified;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id", referencedColumnName = "id")
+    private Brand brand;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    public int getStartYear() {
+        return startYear;
+    }
+    public void setStartYear(int startYear) {
+        this.startYear = startYear;
+    }
+
+    public int getEndYear() {
+        return endYear;
+    }
+    public void setEndYear(int endYear) {
+        this.endYear = endYear;
+    }
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+    public LocalDateTime getModified() {
+        return modified;
+    }
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+
+}
---
+++ src/main/java/bg/softuni/MobLeLeLe/model/entity/Offer.java
@@ -0,0 +1,4 @@
+package bg.softuni.MobLeLeLe.model.entity;
+
+public class Offer {
+}
