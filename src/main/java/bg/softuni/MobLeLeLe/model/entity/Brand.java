76cafa6574c738f26c59d8aff381b67c1a8e9de3
Konstantina Vasileva <konstantinavasileva@gmail.com>
2024-12-11 12:05:17 +0200

    Updated application.yaml files. Added getter and setter methods in the `Brand`. Included JDBC starter dependency in `build.gradle` for enhanced database functionality.

--- src/main/java/bg/softuni/MobLeLeLe/model/entity/Brand.java
+++ src/main/java/bg/softuni/MobLeLeLe/model/entity/Brand.java
@@ -21,4 +21,35 @@ public class Brand {
 
     private LocalDateTime modified;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
 }
