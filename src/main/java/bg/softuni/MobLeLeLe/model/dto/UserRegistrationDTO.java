3131fd66e2b02bdb2908d02bf8e130f50299187f
Konstantina Vasileva <konstantinavasileva@gmail.com>
2024-12-23 16:00:01 +0200

    Create fragment navbar.

--- src/main/java/bg/softuni/MobLeLeLe/model/dto/UserRegistrationDTO.java
+++ src/main/java/bg/softuni/MobLeLeLe/model/dto/UserRegistrationDTO.java
@@ -1,7 +1,19 @@
 package bg.softuni.MobLeLeLe.model.dto;
 
-public record UserRegistrationDTO(String firstName,
-                                  String lastName,
-                                  String password,
-                                  String email) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRegistrationDTO(
+        @NotBlank
+        @Size(min = 3, max = 30)
+        String firstName,
+        @NotBlank
+        @Size(min = 3, max = 30)
+        String lastName,
+        @NotBlank
+        String password,
+        @NotBlank
+        @Email
+        String email) {
 }
