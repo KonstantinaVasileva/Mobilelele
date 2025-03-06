78e1b11fc5c516402b1055090ba97dc6393716c9
Konstantina Vasileva <konstantinavasileva@gmail.com>
2025-03-06 16:44:45 +0200

    try

--- src/main/java/bg/softuni/MobLeLeLe/model/dto/UserRegistrationDTO.java
+++ src/main/java/bg/softuni/MobLeLeLe/model/dto/UserRegistrationDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
-import java.util.Objects;
+
 
 public class UserRegistrationDTO {
     @NotEmpty
